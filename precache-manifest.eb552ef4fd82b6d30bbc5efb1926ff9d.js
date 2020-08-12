f551ffd84b081a70cbc78bbe7e56e989cc64f54b
Nitiksharma31 <69038144+Nitiksharma31@users.noreply.github.com>
2020-08-12 13:24:42 +0530

    Updates

---
+++ precache-manifest.eb552ef4fd82b6d30bbc5efb1926ff9d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d1e9bdcf163c495221e66cb3e2faea",
+    "url": "/Genius_SIH/index.html"
+  },
+  {
+    "revision": "987ee4928efdd86ff915",
+    "url": "/Genius_SIH/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "96d74dd0b39681d9d18d",
+    "url": "/Genius_SIH/static/js/2.a2eeabc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Genius_SIH/static/js/2.a2eeabc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987ee4928efdd86ff915",
+    "url": "/Genius_SIH/static/js/main.6d9440ad.chunk.js"
+  },
+  {
+    "revision": "730f8340d17193b858d5",
+    "url": "/Genius_SIH/static/js/runtime-main.51f9cd53.js"
+  },
+  {
+    "revision": "5c5de0496dcab2e9a5e4bd2a1bd7d69a",
+    "url": "/Genius_SIH/static/media/grouppic.5c5de049.jpg"
+  },
+  {
+    "revision": "a49dfcfa687fbadcc77b85fefd318fae",
+    "url": "/Genius_SIH/static/media/news.a49dfcfa.jpg"
+  },
+  {
+    "revision": "1915017f543513f56d3857deecaf6f3b",
+    "url": "/Genius_SIH/static/media/officialpic.1915017f.jpeg"
+  },
+  {
+    "revision": "1403af580e4aed9c83f2d014e1cd96cc",
+    "url": "/Genius_SIH/static/media/sihbanner2.1403af58.png"
+  }
+]);
